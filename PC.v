3561e84252723a5baf9e7448a591ab8c1e518fab
radwaehab <108034279+radwaehab@users.noreply.github.com>
2025-12-24 18:02:09 +0200

    Update PC.v

--- PC.v
+++ PC.v
@@ -7,13 +7,8 @@ output reg   [31:0]out
 
 always @(posedge clk)
   begin 
-    if(rst)  out= 32'd0;
-    else    
-      begin
-        out<=in;
-        in = out + 32'd4;
-      end
-
+    if(rst)  out<= 32'd0;
+    else     out<=in;
   end
 
 endmodule
