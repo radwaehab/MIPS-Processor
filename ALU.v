3d0c8fc17635618a2cc85b0401ef069ad7256e3a
radwaehab <108034279+radwaehab@users.noreply.github.com>
2025-12-24 18:08:29 +0200

    Update ALU.v

--- ALU.v
+++ ALU.v
@@ -1,7 +1,7 @@
 module ALU (
     input  wire [31:0] A,
     input  wire [31:0] B,
-    input  wire [2:0]  ALUcontrol,
+    input  wire [3:0]  ALUcontrol,
 
     output reg  [31:0] Result,
     output wire Zero,
@@ -13,11 +13,11 @@ module ALU (
     always @(*)
     begin
         case (ALUcontrol)
-            3'b000: Result = A + B;
-            3'b001: Result = A - B;
-            3'b010: Result = A & B;
-            3'b011: Result = A | B;
-            3'b100: Result = ($signed(A) < $signed(B)) ? 32'd1 : 32'd0;
+            3'b0010: Result = A + B;
+            3'b0110: Result = A - B;
+            3'b0000: Result = A & B;
+            3'b0001: Result = A | B;
+        //  3'b100: Result = ($signed(A) < $signed(B)) ? 32'd1 : 32'd0;
             default: Result = 32'd0;
         endcase
     end
