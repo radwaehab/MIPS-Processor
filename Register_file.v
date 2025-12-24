cc9f4170a7530f68fd1443eb026af5721be61d81
radwaehab <108034279+radwaehab@users.noreply.github.com>
2025-12-24 18:06:17 +0200

    Update Register_file.v

--- Register_file.v
+++ Register_file.v
@@ -1,13 +1,13 @@
 module Register_file (
 
 input      clk, RegWrite,
-      wire [2:0]  rs,   //rs
-      wire [2:0]  rt,   //rt
-      wire [2:0]  rd,   //rd
-      wire [31:0] write_data,
+      wire [4:0]  rsA,         //rs address
+      wire [4:0]  rtA,         //rt address
+      wire [4:0]  write_reg,   //rd address
+      wire [31:0] Regwrite_data,
 
-output wire [31:0] Read_data1,
-       wire [31:0] Read_data2
+output wire [31:0] rsV,        //rs value
+       wire [31:0] rtV         //rt value
 );
 
 reg [31:0] registers[31:0];
@@ -16,14 +16,14 @@ reg [31:0] registers[31:0];
 //     write part 
 always @(posedge clk)
   begin
-    if (RegWrite && (rd!=0))
+    if (RegWrite && (write_reg!=0)) //??????
       begin
-       registers[rd] <= write_data;
+       registers[write_reg] <= Regwrite_data;
       end 
   end
 
 //     read part 
-   assign Read_data1= registers[rs];
-   assign Read_data2= registers[rt];
+   assign rsV= registers[rsA];
+   assign rtV= registers[rtA];
 
 endmodule
