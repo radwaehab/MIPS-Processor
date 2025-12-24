e8355e4dbc34a3eb278eb118fabe3312a8dc2bde
radwaehab <108034279+radwaehab@users.noreply.github.com>
2025-12-24 18:07:20 +0200

    Update sign_extd.v

--- sign_extd.v
+++ sign_extd.v
@@ -1,17 +1,11 @@
-module sign_extd(
+ module sign_extd(
 input  wire        extd,
 input  wire    [15:0]in,
 output reg     [31:0]out
 );
-wire o;
-assign o = (~extd)?   {16'h0 ,in} : {16'h0xffff ,in};
-assign out = (32'd4)*o;  // shift left 2
+wire msb;
+assign  msb = in[15];
+assign out =  (extd)?   (msb? {16'h0xffff ,in} : {16'h0 ,in} ) : 
+                        {16'h0 ,in};
 
-
-/*
-alway @*
-  begin
-    if(~extd)  
-  end
-*/
 endmodule
