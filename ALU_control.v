69dfade3dfec6e58331c9d4a6282102e06d32852
radwaehab <108034279+radwaehab@users.noreply.github.com>
2025-12-24 18:02:41 +0200

    Update ALU_control.v

--- ALU_control.v
+++ ALU_control.v
@@ -1,9 +1,30 @@
 module ALU_control(
-input  wire               alu_op,
-input  wire [4:0]         funct,   
-output wire [2:0]      ALUcontrol
+input  wire [1:0]         alu_op,
+input  wire [5:0]         funct,   
+output wire [3:0]      ALUcontrol
 );
-assign funct= instruction[4:0];   // last 5bits from instruction
-assign ALUcontrol = (alu_op)?     :       ;
+always @*
+  begin
+    if (alu_op= 2'b10)  
+      begin
+        case (funct)
+          begin
+            6'b100000 :   ALUcontrol= 4'b0010;  //add
+            6'b100010 :   ALUcontrol= 4'b0110;  //sub
+            6'b100100 :   ALUcontrol= 4'b0000;  //AND
+            6'b100101 :   ALUcontrol= 4'b0001;  //OR
+    //      6'b101010 :   ALUcontrol= 4'b0111;  //slt
+          end
+      end 
+    else 
+       begin
+        case(alu_op)
+          begin
+            2'b00     :   ALUcontrol= 4'b0010;  //add
+            2'b01     :   ALUcontrol= 4'b0110;  //sub
+          end
+       end
+
+  end 
 
 endmodule
