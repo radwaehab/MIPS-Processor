c15ae5b4a6f6a78add3a2cbf0cf55ca2b45c6354
radwaehab <108034279+radwaehab@users.noreply.github.com>
2025-12-26 02:46:31 +0200

    Update Instruction_MEM.v

--- Instruction_MEM.v
+++ Instruction_MEM.v
@@ -1,11 +1,26 @@
 module instruction_MEM(
 
 input wire [31:0] read_address,
-
-output wire[31:0] instruction
-):
+output reg[31:0] instruction
+);
 
 reg [19:0] i_mem [0:255];          //  256x20 instruction memory.
-instruction= i_mem[read_address];
+integer i;
+
+ initial begin
+         //=====initialization=======
+        i_mem[0] = 32'h20080005; // addi $t0, $zero, 5
+        i_mem[1] = 32'h20090003; // addi $t1, $zero, 3
+        i_mem[2] = 32'h01095020; // add  $t2, $t0, $t1
+        i_mem[3] = 32'h012A5822; // sub  $t3, $t1, $t2
+        i_mem[4] = 32'h08000004; // j    4 (infinite loop)
+
+        // Fill rest with NOPs
+                for (i = 5; i < 256; i = i + 1)
+            i_mem[i] = 32'h00000000;
+   // end
+  end
+
+  assign  instruction= i_mem[read_address];
 
 endmodule
